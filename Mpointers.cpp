aa801d0fff92e2b21e7d94daa12554f9b852f6bc
Asrai04 <a.saborio.1@estudiantec.cr>
2025-04-11 00:00:15 -0600

    Funcion que solo envia

--- Mpointers.cpp
+++ Mpointers.cpp
@@ -116,6 +116,50 @@ public:
     return respuesta;
     }
 
+    void Only_Send(const std::string& Mensaje) {
+        WSADATA wsaData;
+    int result = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (result != 0) {
+        std::cerr << "Error al inicializar Winsock. Código de error: " << result << std::endl;
+    }
+
+    // Crear un socket
+    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Error al crear el socket. Código de error: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+    }
+
+    // Dirección del servidor (localhost)
+    sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(puerto);
+    serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    // Conectar al servidor
+    result = connect(clientSocket, (sockaddr*)&serverAddr, sizeof(serverAddr));
+    if (result == SOCKET_ERROR) {
+        std::cerr << "No se pudo conectar al servidor. Código de error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+    // Enviar el mensaje al servidor
+    result = send(clientSocket, Mensaje.c_str(), Mensaje.size(), 0);
+    if (result == SOCKET_ERROR) {
+        std::cerr << "Error al enviar el mensaje. Código de error: " << WSAGetLastError() << std::endl;
+        closesocket(clientSocket);
+        WSACleanup();
+    }
+
+    std::cout << "Mensaje enviado: " << Mensaje << std::endl;
+
+    // Cerrar el socket
+    closesocket(clientSocket);
+    WSACleanup();
+    }
+
+
     int getPuerto() {
         return puerto;
     }
